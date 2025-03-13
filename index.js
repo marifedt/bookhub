fdcb345b530d20c65a05917e5470044d1a9308f6
marifedt-w <57739727+marifedt@users.noreply.github.com>
2025-03-13 15:34:51 -0400

    Add dynamic route for book details and notes

--- index.js
+++ index.js
@@ -55,17 +55,71 @@ const preloadImagesForRendering = async (books) => {
   return Promise.all(preloadPromises);
 };
 
+// Format Dates
+const formatDates = async (collection, propertyName) => {
+  const datesPromises = collection.map(async (item) => {
+    const date = new Date(item[propertyName]);
+    const formattedDate = date.toLocaleDateString('en-US', {
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric',
+    });
+
+    return {
+      ...item,
+      formatted_date: formattedDate,
+    };
+  });
+
+  return Promise.all(datesPromises);
+};
+
 // ROUTES
 app.get('/', async (req, res) => {
   try {
-    const result = await db.query('SELECT * FROM book');
+    const BOOKS_QUERY = `SELECT book.id, book.isbn, book.title, book.read_date, book.rating, book.summary, author.name as "author_name", author.id as "author_id" FROM book
+JOIN author on author.id = book.author_id
+ORDER BY read_date DESC`;
+    const result = await db.query(BOOKS_QUERY);
+
     const books = result.rows;
-
     const bookCoverUrls = await preloadImagesForRendering(books);
+    const booksWithFormattedDates = await formatDates(books, 'read_date');
 
-    res.render('index.ejs', {
-      books,
+    res.render('books/index.ejs', {
+      books: booksWithFormattedDates,
       bookCoverUrls,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.get('/books/:isbn', async (req, res) => {
+  try {
+    const isbn = req.params.isbn;
+    const bookQuery = `SELECT book.id, book.isbn, book.title, book.read_date, book.rating, book.summary, author.name as "author_name", author.id as "author_id" FROM book
+                      JOIN author on author.id = book.author_id
+                      WHERE book.isbn = '${isbn}'
+                      ORDER BY read_date DESC`;
+
+    // Get book details
+    let result = await db.query(bookQuery);
+    const book = result.rows[0];
+    // Get image from Open Library and preload it.
+    const bookCoverUrl = await preloadImagesForRendering([book]);
+    // Format the date
+    const bookWithFormattedDate = await formatDates([book], 'read_date');
+
+    // Get notes for the book
+    result = await db.query(`SELECT * FROM note WHERE book_id = ${book.id}`);
+    const notes = result.rows;
+    const notesWithFormattedDates = await formatDates(notes, 'created_at');
+
+    res.render('books/note.ejs', {
+      book: bookWithFormattedDate[0],
+      notes: notesWithFormattedDates,
+      bookCoverUrl,
     });
   } catch (error) {
     console.log(error);
